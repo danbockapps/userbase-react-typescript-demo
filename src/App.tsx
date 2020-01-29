9848b8803e42ea05401f50414c6b4df3150f6bfb
Dan Bock <danbock@gmail.com>
2020-01-29 16:37:29 -0500

    Step 1: User Registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import userbase, { UserResult } from 'userbase-js'
 
 const App: React.FC = () => {
+  const [user, setUser] = useState<UserResult>()
+
+  useEffect(() => {
+    userbase
+      .init({ appId: process.env.REACT_APP_USERBASE_APP_ID as string })
+      .then(session => session.user && setUser(session.user))
+  }, [])
+
+  const [regForm, setRegForm] = useState<{
+    username?: string
+    password?: string
+  }>({ username: '', password: '' })
+
+  const handleRegInputChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setRegForm({ ...regForm, [event.target.name]: event.target.value })
+
+  const handleRegSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (regForm.username && regForm.password)
+      userbase
+        .signUp({
+          username: regForm.username,
+          password: regForm.password,
+          rememberMe: 'local'
+        })
+        .then((ur: UserResult) => setUser(ur))
+        .catch(err => alert(err))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleRegSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={regForm?.username}
+            onChange={handleRegInputChange}
+          />
+        </label>
+
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={regForm?.password}
+            onChange={handleRegInputChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
